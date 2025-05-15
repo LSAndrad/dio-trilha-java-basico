5a3652d62eb44151241874971d4d8b0178b74ebd
byd-lucas-andrade <byd.lucas.andrade.250199@gmail.com>
2025-05-14 21:04:43 -0300

    Meu projeto sobre fluxo de Contador

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+public class ParametrosInvalidosException extends Exception {
+    // Construtor com mensagem
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
